943c922dd5849fe6fe0a9d6e6733fe7e92ccc0d1
Flenzil <sam.neale100@gmail.com>
2025-09-17 17:22:47 +0100

    (feat) update default spacing between cards

--- src/params.py
+++ src/params.py
@@ -15,7 +15,7 @@ card_width_low = int(card_width_high / 4)
 
 # Parameters in millimetres
 card_width_in_mm = 63
-spacing_default = 0.1
+spacing_default = 0.15
 bleed_default = 3.175
 margin_x_min = 1
 margin_y_min = 1
